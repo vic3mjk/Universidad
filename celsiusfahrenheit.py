013c648658cb052808aa49b247fe15ee826e5b46
vic3mjk <vicente.alons.sm@gmail.com>
2025-05-20 20:05:46 +0000

    fahrenheit

---
+++ celsiusfahrenheit.py
@@ -0,0 +1,4 @@
+def conversor(a):
+    return (a * 9/5) +32
+
+print(conversor(int(input("ingrese num a convertir a fahrenheit"))))
--- funciones lambda.py
+++
@@ -1,6 +0,0 @@
-cubo = lambda x : x** 3
-
-print(cubo(2))
-lista  = [lambda x : x ** 2, lambda x : x ** 0.5]
-for numero in lista:
-    print(numero(9))
